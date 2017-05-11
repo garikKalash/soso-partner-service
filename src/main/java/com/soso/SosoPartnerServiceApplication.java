f43f1cff74f7868aad294e5fa846e03b33c1f4ec
Garik Kalashyan <garik.kalash@gmail.com>
2017-05-11 14:07:36 +0400

    asa

--- src/main/java/com/soso/SosoPartnerServiceApplication.java
+++ src/main/java/com/soso/SosoPartnerServiceApplication.java
@@ -1,12 +1,18 @@
 package com.soso;
 
+import org.apache.catalina.authenticator.jaspic.AuthConfigFactoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.security.auth.message.config.AuthConfigFactory;
 
 @SpringBootApplication
 public class SosoPartnerServiceApplication {
 
     public static void main(String[] args) {
+        if (AuthConfigFactory.getFactory() == null) {
+            AuthConfigFactory.setFactory(new AuthConfigFactoryImpl());
+        }
         SpringApplication.run(SosoPartnerServiceApplication.class, args);
     }
 
